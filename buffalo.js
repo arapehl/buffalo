eb63d07f69252570b8c0e4d38183bfd4a0e358fb
Ara Pehlivanian <ara.pehlivanian@gmail.com>
2015-06-05 10:55:35 -0400

    initial commit

---
+++ buffalo.js
@@ -0,0 +1,50 @@
+function buffalo(BUFFALO, Buffalo) {
+    Buffalo &&( buffalo. buffalo =[,
+    BUFFALO, BUFFALO = Buffalo = buffalo.
+    $Buffalo =-( BUFFALO ==( buffalo.
+    Buffalo = Buffalo ))], buffalo.
+    buffalo [Buffalo++] = buffalo. buffalo, buffalo.
+    BUFFALO = ++Buffalo, buffalo (--Buffalo), buffalo.
+    $Buffalo = ++Buffalo, buffalo. BUFFALO++ );
+    Buffalo = buffalo. Buffalo [buffalo.
+    $Buffalo++ ]; buffalo. Buffalo = BUFFALO? Buffalo?
+    '\012'== Buffalo? buffalo (++ BUFFALO, buffalo.
+    buffalo [++ buffalo. BUFFALO ]=
+    BUFFALO - BUFFALO ): Buffalo
+    ==' '|'\015'== Buffalo ||
+    (Buffalo   )== "c" &  buffalo. Buffalo [buffalo.
+    $Buffalo++ ]== "h" &  buffalo. Buffalo [buffalo.
+    $Buffalo++ ]== "i" &  buffalo. Buffalo [buffalo.
+    $Buffalo++ ]== "c" &  buffalo. Buffalo [buffalo.
+    $Buffalo++ ]== "k" &  buffalo. Buffalo [buffalo.
+    $Buffalo++ ]== "e" &  buffalo. Buffalo [buffalo.
+    $Buffalo++ ]== "n"&&++buffalo. buffalo [buffalo.
+    BUFFALO]? buffalo (BUFFALO)
+    :[ "Error on line "+BUFFALO+": expected 'buffalo'",
+       buffalo. BUFFALO = BUFFALO ++- BUFFALO ]:
+    buffalo. buffalo :( BUFFALO = buffalo.
+    Buffalo[buffalo.BUFFALO], Buffalo? (Buffalo =
+
+    --Buffalo? --Buffalo? --Buffalo? --Buffalo? --Buffalo?
+    --Buffalo? --Buffalo? --Buffalo? --Buffalo?
+    buffalo. BUFFALO++ &&
+    --Buffalo :'&#'+BUFFALO+';': buffalo.
+    Buffalo [buffalo. Buffalo [-- buffalo. BUFFALO ]&&
+    (buffalo. $Buffalo += BUFFALO), --buffalo.
+    BUFFALO ]: buffalo. Buffalo [buffalo.
+    Buffalo [BUFFALO] = buffalo. Buffalo
+    [-- buffalo. BUFFALO ],-- buffalo. BUFFALO ]:
+    buffalo. Buffalo [buffalo. Buffalo [buffalo.
+    $Buffalo++ ]] [BUFFALO]: BUFFALO == buffalo.
+    Buffalo [-- buffalo. BUFFALO ]:
+    BUFFALO*buffalo. Buffalo [-- buffalo.
+    BUFFALO ]: buffalo. Buffalo [-- buffalo.
+    BUFFALO ]- BUFFALO: buffalo. Buffalo [-- buffalo.
+    BUFFALO ]+ BUFFALO: buffalo.
+    BUFFALO ++ && "buffalo", buffalo.
+    Buffalo [buffalo. BUFFALO ]= Buffalo, buffalo
+    ()): BUFFALO );
+
+    return buffalo.
+    Buffalo
+}
